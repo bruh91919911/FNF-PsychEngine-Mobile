0e36c38f431dad6bfd535d4f20915e2e41e70f86
bruh91919911 <156580719+bruh91919911@users.noreply.github.com>
2024-06-09 14:43:26 +0300

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -399,6 +399,7 @@ class PlayState extends MusicBeatState
 			case 'school': new states.stages.School(); //Week 6 - Senpai, Roses
 			case 'schoolEvil': new states.stages.SchoolEvil(); //Week 6 - Thorns
 			case 'tank': new states.stages.Tank(); //Week 7 - Ugh, Guns, Stress
+			case 'house': new.states.stages.house(); //warmup, house, bla bla
 		}
 
 		if(isPixelStage) {
