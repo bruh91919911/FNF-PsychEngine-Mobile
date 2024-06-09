3ed8a1e2c83864c4e618c4953823822aa67c8c13
bruh91919911 <156580719+bruh91919911@users.noreply.github.com>
2024-06-09 22:55:13 +0300

    Update House.hx

--- source/states/stages/House.hx
+++ source/states/stages/House.hx
@@ -7,19 +7,19 @@ class House extends BaseStage
 {
 	override function create()
 	{
-		var bg:BGSprite = new BGSprite('bg', -600, -300, Paths.image('davehouse/bg'), 0.6, 0.6);
+		var bg:BGSprite = new BGSprite('davehouse/bg', -600, -300, 0.6, 0.6);
 		add(bg);
 		
-		var stageHills:BGSprite = new BGSprite('stageHills', -834, -159, Paths.image('davehouse/hills'), 0.7, 0.7);
+		var stageHills:BGSprite = new BGSprite('davehouse/hills', -834, -159, 0.7, 0.7);
 		add(stageHills);
 		
-		var grassbg:BGSprite = new BGSprite('grassbg', -1205, 580, Paths.image('davehouse/grassbg'));
+		var grassbg:BGSprite = new BGSprite('davehouse/grassbg', -1205, 580);
 		add(grassbg);
 		
-		var gate:BGSprite = new BGSprite('bg', -755, 250, Paths.image('davehouse/gate'));
+		var gate:BGSprite = new BGSprite('davehouse/gate', -755, 250);
 		add(gate);
 		
-		var stageFront:BGSprite = new BGSprite('bg', -832, 505, Paths.image('davehouse/grass'));
+		var stageFront:BGSprite = new BGSprite('davehouse/grass', -832, 505);
 		add(stageFront);
 	}
 }
